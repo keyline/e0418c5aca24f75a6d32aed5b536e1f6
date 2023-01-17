3d74a5b58cf2c80e44f8778596f5ddc988a67263
Shuvadeep Chakraborty <shuvadeep@keylines.net>
2023-01-17 18:53:43 +0530

    social login use cases are resolved

--- app/Views/front/layout-front.php
+++ app/Views/front/layout-front.php
@@ -40,13 +40,20 @@ $NO_IMAGE_URL   = getenv('NO_IMAGE_URL');
                 $($(this).data('modal')).modal();
                 return false;
                 });
+
+                $(document).on('click', 'button#disconnect',function(){
+                    let user= {user: $(this).data('wantlogout')};
+                    handleLogOut(user).then(function(response){
+                        //handle ui
+                        // convert to JQuery object
+                        const logoutdata = JSON.parse(response);
+                        afterLoggedinUIHandle(logoutdata);
+                    });
+                });
             });
         </script>
         <script type="text/javascript">
-            (function (elemID) { 
-                $('#' + elemID).modal(); 
-            })("socialbtn-modal");
-
+            
             $(window).scroll(function() 
                 {
                     if ($(this).scrollTop() > 1)
@@ -154,79 +161,6 @@ $NO_IMAGE_URL   = getenv('NO_IMAGE_URL');
             }
             });
 
-            // });
-
-            
-
-           //facebook login
-           window.fbAsyncInit = function() {
-            // FB JavaScript SDK configuration and setup
-            FB.init({
-              appId      : '564063147297627', // FB App ID
-              cookie     : true,  // enable cookies to allow the server to access the session
-              xfbml      : true,  // parse social plugins on this page
-              version    : 'v15.0' // use graph api version 2.10
-            });
-            
-            // Check whether the user already logged in
-            FB.getLoginStatus(function(response) {
-                if (response.status === 'connected') {
-                    //display user data
-                    getFbUserData();
-                }
-            });
-        };
-
-        // Load the JavaScript SDK asynchronously
-        (function(d, s, id) {
-            var js, fjs = d.getElementsByTagName(s)[0];
-            if (d.getElementById(id)) return;
-            js = d.createElement(s); js.id = id;
-            js.src = "//connect.facebook.net/en_US/sdk.js";
-            fjs.parentNode.insertBefore(js, fjs);
-        }(document, 'script', 'facebook-jssdk'));
-
-        // Facebook login with JavaScript SDK
-        function fbLogin() {
-            FB.login(function (response) {
-                if (response.authResponse) {
-                    // Get and display the user profile data
-                    getFbUserData();
-                } else {
-                    document.getElementById('status').innerHTML = 'User cancelled login or did not fully authorize.';
-                }
-            }, {scope: 'email'});
-        }
-
-        // Fetch the user profile data from facebook
-        function getFbUserData(){
-            FB.api('/me', {locale: 'en_US', fields: 'id,first_name,last_name,email,link,gender,locale,picture,cover'},
-            function (response) {
-                //document.getElementById('fbloginbutton').setAttribute("onclick","fbLogout()");
-                //document.getElementById('fbloginbutton').innerHTML = 'Logout from Facebook';
-                //document.getElementById('disconnect').style.display='block';
-                //document.getElementById('disconnect').innerHTML = 'Logout from Facebook';
-                //document.getElementById('status').innerHTML = 'Thanks for logging in, ' + response.first_name + '!';
-                //document.getElementById('userData').innerHTML = '<div style="position: relative;"><img src="" /><img style="position: absolute; top: 90%; left: 25%;" src="'+response.picture.data.url+'"/></div><p><b>FB ID:</b> '+response.id+'</p><p><b>Name:</b> '+response.first_name+' '+response.last_name+'</p><p><b>Email:</b> '+response.email+'</p><p><b>Gender:</b> '+response.gender+'</p><p><b>Locale:</b> '+response.locale+'</p><p><b>Profile Link:</b> <a target="_blank" href="'+response.link+'">click to view profile</a></p>';
-                //document.getElementById('user-img').src= response.picture.data.url;
-//                 const closeModalsBtn = document.querySelectorAll(".modal-box__exit-button");
-
-//                 closeModalsBtn.forEach(closeBtn=>{
-//   closeBtn.addEventListener('click' , (e) =>{
-//     e.currentTarget.parentElement.parentElement.parentElement.parentElement.style.display = 'none'
-//   })
-// });
-
-                // Save user data
-                saveUserData(response);
-            });
-        }
-
-        // Save user data to the database
-        function saveUserData(userData){
-            $.post("<?php echo base_url('Social_login/saveUsersData'); ?>", {oauth_provider:'facebook', userData: JSON.stringify(userData)}, function(data){ return true});
-        }
-
         // Logout from facebook
         //<img src="<?php //echo base_url('assets/images/fblogin.png');?>"/>
         function fbLogout() {
@@ -305,6 +239,10 @@ $NO_IMAGE_URL   = getenv('NO_IMAGE_URL');
                     if (response.status === 'connected') {
                         //display user data
                         getFbUserData();
+                    }else{
+                        //soft login modal
+                        $("#socialbtn-modal").modal();
+
                     }
                 });
             };
@@ -334,20 +272,26 @@ $NO_IMAGE_URL   = getenv('NO_IMAGE_URL');
             function getFbUserData(){
                 FB.api('/me', {locale: 'en_US', fields: 'id,first_name,last_name,email,link,gender,locale,picture,cover'},
                 function (response) {
-                    document.getElementById('fbloginbutton').setAttribute("onclick","fbLogout()");
-                    document.getElementById('fbloginbutton').innerHTML = 'Logout from Facebook';
-                    document.getElementById('status').innerHTML = 'Thanks for logging in, ' + response.first_name + '!';
-                    document.getElementById('userData').innerHTML = '<div style="position: relative;"><img src="" /><img style="position: absolute; top: 90%; left: 25%;" src="'+response.picture.data.url+'"/></div><p><b>FB ID:</b> '+response.id+'</p><p><b>Name:</b> '+response.first_name+' '+response.last_name+'</p><p><b>Email:</b> '+response.email+'</p><p><b>Gender:</b> '+response.gender+'</p><p><b>Locale:</b> '+response.locale+'</p><p><b>Profile Link:</b> <a target="_blank" href="'+response.link+'">click to view profile</a></p>';
-                    document.getElementById('user-img').src= response.picture.data.url;
+                    // document.getElementById('fbloginbutton').setAttribute("onclick","fbLogout()");
+                    // document.getElementById('fbloginbutton').innerHTML = 'Logout from Facebook';
+                    // document.getElementById('status').innerHTML = 'Thanks for logging in, ' + response.first_name + '!';
+                    // document.getElementById('userData').innerHTML = '<div style="position: relative;"><img src="" /><img style="position: absolute; top: 90%; left: 25%;" src="'+response.picture.data.url+'"/></div><p><b>FB ID:</b> '+response.id+'</p><p><b>Name:</b> '+response.first_name+' '+response.last_name+'</p><p><b>Email:</b> '+response.email+'</p><p><b>Gender:</b> '+response.gender+'</p><p><b>Locale:</b> '+response.locale+'</p><p><b>Profile Link:</b> <a target="_blank" href="'+response.link+'">click to view profile</a></p>';
+                    // document.getElementById('user-img').src= response.picture.data.url;
                     
                     // Save user data
-                    saveUserData(response);
+                    saveUserData(response).then(function(handleSession){
+                        // convert to JQuery object
+                        const sessionObj = JSON.parse(handleSession);
+                        afterLoggedinUIHandle(sessionObj);
+
+                    });
+                    
                 });
             }
 
             // Save user data to the database
             function saveUserData(userData){
-                $.post("<?php echo base_url('Social_login/saveUsersData'); ?>", {oauth_provider:'facebook', userData: JSON.stringify(userData)}, function(data){ return true; });
+                return $.post("<?php echo base_url('Social_login/saveUsersData'); ?>", {oauth_provider:'facebook', userData: JSON.stringify(userData)},);
             }
 
             // Logout from facebook
@@ -361,6 +305,33 @@ $NO_IMAGE_URL   = getenv('NO_IMAGE_URL');
                     document.getElementById('user-img').src='<?=$ASSETS_URL?>images/man.jpg';
                 });
             }
+
+            function afterLoggedinUIHandle(response){
+                debugger;
+                let parent= $('.man-img');
+                if(response.sess_logged_in){
+                    //closing the modal
+                    $.modal.close();
+                    //adding user image
+                    $(parent).find('img').attr('src', response.user_profile_img);
+                    $("<div>", {
+                        'class':"dropdown-content"
+                    }).append(`<button name="button" id="disconnect" data-wantlogout="${response.logged_in_id}">Log Out</button>`
+                    ).append('<a href="javascript:void(0);">Welcome ' + response.userfullname + '</a>'
+                    ).appendTo(".dropdown");
+                }else{
+                    //logout ui behaviour
+                    $(parent).find('img').attr('src', response.noimage);
+                    //remove user info dropdown
+                    $(parent).find('.dropdown-content').remove();
+                    
+                }
+             
+                }
+
+             function handleLogOut(user){
+                return $.post("<?php echo base_url('Social_login/logmeOut'); ?>", {action: 'logout', user_data: JSON.stringify(user)}, );
+             }   
 
             // functio for countdown
             function getCountdown(targetDateTime, idSelector){
